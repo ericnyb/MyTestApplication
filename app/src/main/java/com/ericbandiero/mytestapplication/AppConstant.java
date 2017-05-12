db443b2d695bcbbfa34acd272eaa68de3a214ed6
ericnyb <ericnyb@aol.com>
2017-05-12 12:10:21 -0400

    Feature1 to push 2

--- app/src/main/java/com/ericbandiero/mytestapplication/AppConstant.java
+++ app/src/main/java/com/ericbandiero/mytestapplication/AppConstant.java
@@ -4,6 +4,7 @@ package com.ericbandiero.mytestapplication;
  * Created by ${"Eric Bandiero"} on 4/6/2017.
  * Our app - will not merge this yet.
  * Added new feature that we push to remote.
+ * Will add another pull request.
  */
 
 class AppConstant {
