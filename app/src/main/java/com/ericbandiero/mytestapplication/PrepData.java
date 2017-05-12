051ab20880a60e6d163497e7bc40569f1861e369
ericnyb <ericnyb@aol.com>
2017-05-12 09:43:45 -0400

    This should not merge when we use cheery pick.
    (cherry picked from commit f28bbe2)

--- app/src/main/java/com/ericbandiero/mytestapplication/PrepData.java
+++ app/src/main/java/com/ericbandiero/mytestapplication/PrepData.java
@@ -12,11 +12,18 @@ import java.util.Map;
 
 /**
  * Created by ${"Eric Bandiero"} on 4/9/2017.
+ * Interface to prepare data.
  */
 
 public class PrepData implements IPrepDataExpandableList {
+	/**
+	 * This returns a list of Lib_ExpandableDataWithIds
+	 * Commit to be chery picked for merge.
+	 * @return
+	 */
 	@Override
 	public List<Lib_ExpandableDataWithIds> prepData() {
 		return null;
 	}
+	//This one one show up when we use cherry pick for previous.
 }
