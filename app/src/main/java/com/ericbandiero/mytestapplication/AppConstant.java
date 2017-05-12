d5997cc814f832f29e28193f5001cf3fcc2979ac
ericnyb <ericnyb@aol.com>
2017-05-12 10:40:09 -0400

    Squashed commit of the following:
    commit dd9f6704da67ed2138f9153fd95f5b18c02c228d
    Author: ericnyb <ericnyb@aol.com>
    Date:   Fri May 12 10:35:20 2017 -0400
    
        Minor change 4
    
    commit 96bb839b780ca5441557321123144fa4abfefd23
    Author: ericnyb <ericnyb@aol.com>
    Date:   Fri May 12 10:21:16 2017 -0400
    
        Minor change 3 final
    
    commit 965d2e087465d262f6ff03b5368d7da74078e6f8
    Author: ericnyb <ericnyb@aol.com>
    Date:   Fri May 12 10:20:35 2017 -0400
    
        Minor change 2
    
    commit 24c75360125a7bd059f379eb72f787547a3716cd
    Author: ericnyb <ericnyb@aol.com>
    Date:   Fri May 12 10:19:56 2017 -0400
    
        Minor change 1

--- app/src/main/java/com/ericbandiero/mytestapplication/AppConstant.java
+++ app/src/main/java/com/ericbandiero/mytestapplication/AppConstant.java
@@ -3,6 +3,10 @@ package com.ericbandiero.mytestapplication;
 /**
  * Created by ${"Eric Bandiero"} on 4/6/2017.
  * Our app - will not merge this yet.
+ * Minor change 1
+ * Minor change 2
+ * Minor change 3 to pick above 2 will be squashed.
+ * Minor change 4
  */
 
 class AppConstant {
