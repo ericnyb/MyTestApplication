d9046cf782877c907880676ad9a1b5fabdd25ed2
ericnyb <ericnyb@aol.com>
2017-05-12 11:30:33 -0400

    Removed bogus comments

--- app/src/main/java/com/ericbandiero/mytestapplication/AppConstant.java
+++ app/src/main/java/com/ericbandiero/mytestapplication/AppConstant.java
@@ -3,13 +3,6 @@ package com.ericbandiero.mytestapplication;
 /**
  * Created by ${"Eric Bandiero"} on 4/6/2017.
  * Our app - will not merge this yet.
- * Minor change 1
- * Minor change 2
- * Minor change 3 to pick above 2 will be squashed.
- * Minor change 4
- * Minor change 5
- * Minor change 6
- * Minor change 7
  */
 
 class AppConstant {
