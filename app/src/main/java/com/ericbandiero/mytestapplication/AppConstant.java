b3c39388afafa7a485f0eb5865f0ab31742dbcef
ericnyb <ericnyb@aol.com>
2017-05-12 20:35:59 -0400

    Clenup

--- app/src/main/java/com/ericbandiero/mytestapplication/AppConstant.java
+++ app/src/main/java/com/ericbandiero/mytestapplication/AppConstant.java
@@ -2,12 +2,7 @@ package com.ericbandiero.mytestapplication;
 
 /**
  * Created by ${"Eric Bandiero"} on 4/6/2017.
- * Our app - will not merge this yet.
- * Added new feature that we push to remote.
- * Will add another pull request.
- * New note - committed but not pushed.
- * New pull try!
- * Pull request redo
+  * Pull request redo
  */
 
 class AppConstant {
